c8597ad599b097016c2acced2338f34ce41eadc0
chenkai <chenkai@buckyos.com>
2022-10-13 19:26:39 +0800

    Add cyfs tools Execution permission

--- dmg/ARM/package/post-install.sh
+++ dmg/ARM/package/post-install.sh
@@ -1,27 +1,8 @@
 #!/bin/sh
 
-# PLIST=${HOME}/Library/LaunchAgents/com.cyfs.runtime-monitor.plist
-# current_user=${USER}
-# current_user_path=${HOME}
-# user=${current_user_path:7}
-# echo user ${user}
-# whoami=whoami
-# echo current user name = ${current_user}
-# echo current user path = ${current_user_path}
+readonly RUNTIME_DIR=${HOME}/Library/Application\ Support/cyfs/services/runtime/tools
 
-# if [ -f ${PLIST} ]; then
-#     sed -i '' 's/whoami/'${user}'/g' ${PLIST}
-# 	echo /bin/launchctl load -w ${PLIST}
-
-#     chmod 600 ${PLIST}
-# 	/bin/launchctl load -w ${PLIST}
-
-# 	runtime_process_id=`ps -ef | grep cyfs-runtime | grep -v "grep" | awk '{print $2}'`
-#     if [[ -n ${runtime_process_id} ]]; then
-#         echo cyfs-runtime pid is running , pid is $runtime_process_id.
-#     else
-#         echo cyfs-runtime process is not running, please check.
-#     fi
-# fi
+chmod +x cyfs-client
+chmod +x pack-tools
 
 exit 0
